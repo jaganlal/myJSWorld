d26b88d4e8e0dbe6964753ad268b7004aee41a8b
Jaganlal Thoppe <jaganlal@gmail.com>
2017-02-11 09:10:39 -0500

    with arrow functions and promise.all
    with arrow functions and promise.all

--- promises.js
+++ promises.js
@@ -3,23 +3,25 @@
 
   + function() { thoppesJS.console.log("********** start promises **********"); }()
 
-  let firstMethod = function fm() {
-    let promise = new Promise(function p(resolve, reject) {
+  let firstMethod = () => {
+    let promise = new Promise((resolve, reject) => {
       setTimeout(function sto() {
         thoppesJS.console.log('first method completed');
         resolve({data: 50});
-      }, 2000);
+      }, 1500);
     });
 
     return promise;
   };
 
-  let secondMethod = function sm() {
-    let promise = new Promise(function p(resolve, reject) {
-      a.b = 10;
+  let secondMethod = (response) => {
+    var response = response;
+    var summedupdata = response.data+10;
+    let promise = new Promise((resolve, reject) => {
+      // a.b = 10;
       setTimeout(function sto() {
         thoppesJS.console.log('second method completed');
-        //resolve({data: 'secondMethod'});
+        resolve({data: summedupdata});
         //reject({data: {error: 'secondMethod'}});
       }, 1000);
     });
@@ -27,25 +29,49 @@
     return promise;
   };
 
-  let thirdMethod = function tm() {
-    let promise = new Promise(function p(resolve, reject) {
+  let thirdMethod = (response) => {
+    var summedupdata = response.data+20;
+    let promise = new Promise((resolve, reject) => {
       setTimeout(function sto() {
         thoppesJS.console.log('third method completed');
-        resolve({data: 'thirdMethod'});
-      }, 3000);
+        resolve({data: summedupdata});
+      }, 1700);
     });
 
     return promise;
   };
 
-  let exceptionHandler = function eh(e) {
+  let genericErrorHandlerMethod = (e) => {
+    thoppesJS.console.log('Error in: '+e.message);
+  };
+
+  let logMethod = (response) => {
+    thoppesJS.console.log(JSON.stringify(response));
+  };
+
+  let exceptionHandler = (e) => {
     thoppesJS.console.log('Exception in: '+e.message);
   }
 
   firstMethod()
-    .then(secondMethod)
-    .then(thirdMethod)
+    .then(secondMethod, genericErrorHandlerMethod)
+    .then(thirdMethod, genericErrorHandlerMethod)
+    .then(logMethod, genericErrorHandlerMethod)
     .catch(exceptionHandler);
+
+  //Different speed async operations
+  let slow = new Promise((resolve) => {
+    setTimeout(resolve, 200, 'slow');
+  });
+  let instant = 'instant';
+  let quick = new Promise((resolve) => {
+    setTimeout(resolve, 50, 'quick');
+  });
+
+  //The order is preserved regardless of what resolved first
+  Promise.all([slow, instant, quick]).then((responses) => {
+    responses.map(response => thoppesJS.console.log(response));
+  });
 
   + function() { thoppesJS.console.log("********** end promises **********"); }()
 }());
